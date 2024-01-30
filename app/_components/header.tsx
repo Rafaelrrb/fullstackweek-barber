965cb527c37c15f83c36fd25ddf0c5df75dec9f1
Rafaelrrb <rafaeborba@gmail.com>
2024-01-30 18:43:45 -0300

    feat: add initial header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+export function Header() {
+  return(
+    <Card>
+      <CardContent className="p-5 justify-between flex flex-row items-center">
+        <Image alt="FSW Barber" src="/logo.png" height={22} width={120}/>
+        <Button variant="outline" size="icon" className="h-8 w-8">
+          <MenuIcon size={18}/>
+        </Button>
+      </CardContent>
+    </Card>
+  );
+} 
